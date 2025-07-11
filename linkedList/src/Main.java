fc05b23d8b78fc2037af3f5162899b4b21e20198
72aminue <167484027+72aminue@users.noreply.github.com>
2025-07-11 18:10:10 +0600

    commit
    new commit

---
+++ linkedList/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        singleLinkedList sLL=new singleLinkedList();
+        sLL.createSingleLinkedList(5);
+
+        sLL.insertInLinkedList(6,1);
+        sLL.insertInLinkedList(7,2);
+        sLL.insertInLinkedList(8,3);
+        sLL.insertInLinkedList(9,4);
+        sLL.traverseSingleLinkedList();
+        sLL.deleteEntireLinkedList();
+        sLL.traverseSingleLinkedList();
+
+    }
+
+}
+
---
+++ linkedList/src/Node.java
@@ -0,0 +1,4 @@
+public class Node {
+    public int value;//value of the node;
+    public Node next;//this is pointing to the next node
+}
